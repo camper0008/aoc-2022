056bbdde0ec730bae6681e45dc2c4ec1550d541d
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2022-12-05 17:15:03 +0100

    [04]

---
+++ 04/src/two.rs
@@ -0,0 +1,40 @@
+use crate::prelude::*;
+
+/// # Panics
+///
+/// Will panic if unable to open file
+
+pub fn print_result(reader: BufReader<File>) {
+    println!(
+        "{}",
+        reader
+            .lines()
+            .map(|pair| {
+                let mut ranges = pair
+                    .unwrap()
+                    .split(',')
+                    .map(|elf| {
+                        let mut range = elf
+                            .split('-')
+                            .map(|r| r.parse::<u32>().unwrap())
+                            .collect::<Vec<_>>();
+                        let end = range.pop().unwrap();
+                        let start = range.pop().unwrap();
+                        (start, end)
+                    })
+                    .collect::<Vec<(u32, u32)>>();
+                let (start0, end0) = ranges.pop().unwrap();
+                let (start1, end1) = ranges.pop().unwrap();
+                if (start0..=end0).contains(&start1)
+                    || (start0..=end0).contains(&end1)
+                    || (start1..=end1).contains(&start0)
+                    || (start1..=end1).contains(&end0)
+                {
+                    1
+                } else {
+                    0
+                }
+            })
+            .sum::<u32>()
+    );
+}
