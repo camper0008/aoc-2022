3640c4531eabf4a1e9e360c41b8383fbdf4be7a0
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2022-12-06 12:25:06 +0100

    [06]

---
+++ 06/src/one.rs
@@ -0,0 +1,27 @@
+use crate::prelude::*;
+
+pub fn print_result(reader: BufReader<File>) {
+    let mut used_chars = VecDeque::new();
+    let mut iter = reader.bytes().enumerate();
+    println!(
+        "{}",
+        loop {
+            if let Some((i, Ok(b))) = iter.next() {
+                if used_chars.contains(&b) {
+                    loop {
+                        if let Some(el) = used_chars.pop_front() {
+                            if el == b {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                used_chars.push_back(b);
+                if used_chars.len() >= 4 {
+                    break i + 1;
+                }
+            }
+        }
+    );
+}
---
+++ 06/src/prelude.rs
@@ -0,0 +1,3 @@
+pub use std::collections::VecDeque;
+pub use std::fs::File;
+pub use std::io::{BufRead, BufReader, Read};
